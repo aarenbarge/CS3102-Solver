0dd6e747696b6b29d9daee3228908e849a64a7a0
aarenbarge <barge.aaren.signup@gmail.com>
2014-05-01 09:57:44 -0400

    Display Routine working!

--- drawbox.py
+++ drawbox.py
@@ -1,7 +1,85 @@
 from PIL import Image, ImageDraw
+import sys
 
+file = sys.argv[1]
+num  = int(sys.argv[2])
 
-im = Image.new('RGBA', (400, 400), (255, 255, 255, 0)) 
-draw = ImageDraw.Draw(im) 
-draw.line((100,200, 150,300), fill=128)
-im.show()
+f = open(file, 'r')
+content = f.readlines()
+f.close()
+
+num_1 = int(content[0])
+if num_1 == 0:
+    print "No Solutions Were Found!"
+    sys.exit()
+if num_1 < num:
+    num = num_1
+if num < num_1:
+    print str(num_1) + " solutions found, only showing " + str(num)
+    
+size = 50
+buff = 100 
+    
+ints = content[1].split(' ')
+width = int(ints[0])
+height = int(ints[1])
+
+for x in range(0,num):
+    start = 2 + (height + 1)*x
+    end = start + height
+    values = [[0 for x in xrange(width)] for x in xrange(height)]
+    for y in range(start,end):
+        row = content[y].split(' ')
+        for n in range(0,width):
+            values[y-start][n] = int(row[n])
+    im = Image.new('RGBA', (width * size + 2*buff, height * size + 2*buff), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(im) 
+    for y in range(0,height):
+        for n in range (0,width):
+            value = int(values[y][n])
+            if value != 0:
+                if y == 0:
+                    draw.line((n*size + buff,buff,(n+1)*size+buff,buff), fill=128)
+                if y == height - 1:
+                    draw.line((n*size + buff,(y+1)*size + buff,(n+1)*size + buff, (y+1)*size+buff), fill=128)
+                    if n != width -1:
+                        if value != values[y][n+1]:
+                            draw.line(((n+1)*size+buff,y*size+buff,(n+1)*size+buff,(y+1)*size+buff), fill=128)
+                if n == 0:
+                    draw.line((buff,y*size + buff,buff,(y+1)*size+buff), fill=128)
+                if n == width - 1:
+                    draw.line(((n+1)*size + buff, y*size+buff,(n+1)*size+buff,(y+1)*size+buff), fill=128)
+                    if y != height-1:
+                        if value != values[y+1][n]:
+                            draw.line((n*size+buff,(y+1)*size+buff,(n+1)*size+buff,(y+1)*size+buff),fill=128)
+                if y != height -1:
+                    if n != width-1:
+                        if value != values[y+1][n]:
+                            draw.line((n*size+buff,(y+1)*size+buff,(n+1)*size + buff, (y+1)*size+buff), fill=128)
+                        if value != values[y][n+1]:
+                            draw.line(((n+1)*size+buff,y*size+buff,(n+1)*size+buff,(y+1)*size+buff),fill=128)
+                if y != 0:
+                    if n != 0:
+                        if value != values[y-1][n]:
+                            draw.line((n*size+buff,y*size+buff,(n+1)*size+buff,y*size+buff),fill=128)
+                        if value != values[y][n-1]:
+                            draw.line((n*size+buff,y*size+buff,n*size+buff,(y+1)*size+buff),fill=128)
+    im.show()
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
--- solve.sh
+++ solve.sh
@@ -4,13 +4,12 @@ if [ -z "$1" ]; then
 	echo "You must atleast specify an input file!"
 else
 	if [ -f $1 ]; then
-		make
-		./a.out $1
-		./a.out $1
-		./a.out $1
-		./a.out $1
-		python drawbox.py
-		echo "Ran 4 times!"
+		make	
+		if [ -z $2 ]; then
+			z=0
+		fi
+		./a.out $1 $2
+		python drawbox.py c_output.txt 5
 	else
 		echo "That was not a valid input file!"
 	fi
